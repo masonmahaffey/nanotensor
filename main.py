5df7bb6e9548c4b135b477d958c3ce1d7f40336a
Mason Mahaffey <masonmahaffey00@gmail.com>
2023-12-24 23:58:00 -0500

    fixed an output activation function bug where we were using ReLU for the output layer instead of a linear activation function, this prevented the network from training on as well as predicting negative values

--- main.py
+++ main.py
@@ -4,6 +4,10 @@ import numpy as np
 def relu(x):
     return np.maximum(0, x)
 
+# Linear activation function
+def linear(x):
+    return x
+
 # Mean Squared Error loss
 def mse_loss(y_true, y_pred):
     return ((y_true - y_pred) ** 2).mean()
@@ -14,9 +18,9 @@ class SGD:
         self.learning_rate = learning_rate
 
     def update(self, layer):
-        layer.weights -= self.learning_rate * layer.gradient_weights
+        layer.weights -= self.learning_rate * layer.weight_gradients
         # Corrected bias update
-        layer.bias -= self.learning_rate * layer.gradient_bias.reshape(layer.bias.shape)
+        layer.bias -= self.learning_rate * layer.bias_gradients.reshape(layer.bias.shape)
 
 # Define a simple linear layer
 class Layer:
@@ -26,19 +30,20 @@ class Layer:
         self.input = None
         self.output = None
         self.weight_gradients = None
-        self.bias_gradient = None
+        self.bias_gradients = None
 
     def forward(self, input):
         self.input = input
-        # You need to transpose matrices to ensure that the dimensions match up the dot product
+        # This creates a dense layer connecting every neuron in the previous layer to the current layer
+        # via a weight matrix of size input_size x output_size
         self.output = np.dot(input, self.weights) + self.bias
         return self.output
 
     def backward(self, gradient_output):
         # Compute gradients for weights and bias
-        print(self.input.shape)
+        # You need to transpose matrices to ensure that the dimensions match up the dot product
         self.weight_gradients = np.dot(self.input.T, gradient_output)
-        self.bias_gradient = np.sum(gradient_output, axis=0, keepdims=True)
+        self.bias_gradients = np.sum(gradient_output, axis=0, keepdims=True)
         # Compute gradient with respect to input (for passing to previous layer)
         gradient_input = np.dot(gradient_output, self.weights.T)
         return gradient_input
@@ -50,10 +55,15 @@ class NanoTensor:
         self.optimizer = SGD(learning_rate=lr)
 
     def predict(self, x):
-        for layer in self.layers:
-            # I think there is a bug here given the output could never be -1 if the output layer
-            # is a relu activation function.
-            x = relu(layer.forward(x))
+        # Here we need to use a different activation function than ReLU for the output
+        # layer or else our network will not be able to predict negative values due to
+        # the way ReLU works, it is more performant but the tradeoff is that it cannot
+        # output negative values, only positive
+        for index, layer in enumerate(self.layers):
+            if index == len(self.layers) - 1:
+                x = linear(layer.forward(x))
+            else:
+                x = relu(layer.forward(x))
         return x
 
     def compute_loss(self, y_true, y_pred):
@@ -69,12 +79,12 @@ class NanoTensor:
 
 # Example Usage
 learning_rate = 0.001
-layers=[Layer(2, 5), Layer(5, 5), Layer(5, 5), Layer(5, 1)]
+layers=[Layer(2, 5), Layer(5, 10), Layer(10, 5), Layer(5, 1)]
 net = NanoTensor(layers, learning_rate)
 
 # XOR Example
-input_data = np.array([[0, 1], [1, 0], [0, 0], [1, 1]])
-target = np.array([[1], [1], [0], [1]])
+input_data = np.array([[0, -1], [-1, 0], [0, 0], [-1, -1]])
+target = np.array([[-1], [-1], [0], [-1]])
 
 # Training loop
 epochs = 100000
