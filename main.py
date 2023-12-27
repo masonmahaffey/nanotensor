01d704b9d3fd87606f8813dec8c72059e3a3d827
Mason Mahaffey <masonmahaffey00@gmail.com>
2023-12-27 17:58:16 -0500

    Implemented batch normalization to normalize the outputs of each layer within the network. This helps with exploding gradients and vanishing gradients.

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ import numpy as np
 def relu(x):
     return np.maximum(0, x)
 
+def gelu(x):
+    return 0.5 * x * (1 + np.tanh(np.sqrt(2 / np.pi) * (x + 0.044715 * x**3)))
+
 # Leaky ReLU
 def leaky_relu(x, alpha=0.01):
     return np.where(x > 0, x, alpha * x)
@@ -26,42 +29,68 @@ class SGD:
         self.learning_rate = learning_rate
 
     def update(self, layer):
-        # Decrements because you are trying to descend the gradient to the minima
         layer.weights -= self.learning_rate * layer.weight_gradients
         layer.bias -= self.learning_rate * layer.bias_gradients.reshape(layer.bias.shape)
 
+        if layer.use_batchnorm:
+            layer.gamma -= self.learning_rate * layer.gamma_gradients
+            layer.beta -= self.learning_rate * layer.beta_gradients
+
 # Define a simple linear layer
 class Layer:
-    def __init__(self, input_size, output_size):
-        # With the example it's a 2x5 matrix of weights with each input feature having one neuron with a connection
+    def __init__(self, input_size, output_size, use_batchnorm=False):
         self.weights = np.random.randn(input_size, output_size) * 0.1
         self.bias = np.zeros(output_size)
+        self.use_batchnorm = use_batchnorm
+
+        # Batch normalization parameters
+        if use_batchnorm:
+            self.gamma = np.ones(output_size)
+            self.beta = np.zeros(output_size)
+            self.running_mean = np.zeros(output_size)
+            self.running_var = np.zeros(output_size)
+
         self.input = None
         self.output = None
+        self.normalized_output = None
+        self.batch_mean = None
+        self.batch_var = None
         self.weight_gradients = None
         self.bias_gradients = None
+        self.gamma_gradients = None
+        self.beta_gradients = None
 
     def forward(self, input):
         self.input = input
-        # Creates a dense layer and allows us to compute all samples at once
         self.output = np.dot(input, self.weights) + self.bias
+
+        if self.use_batchnorm:
+            self.batch_mean = np.mean(self.output, axis=0)
+            self.batch_var = np.var(self.output, axis=0)
+            self.normalized_output = (self.output - self.batch_mean) / np.sqrt(self.batch_var + 1e-7)
+            self.output = self.gamma * self.normalized_output + self.beta
+
+            # Update running statistics for inference
+            self.running_mean = 0.9 * self.running_mean + 0.1 * self.batch_mean
+            self.running_var = 0.9 * self.running_var + 0.1 * self.batch_var
+
         return self.output
 
     def backward(self, gradient_output):
-        # Compute the gradient of each weight with respect to the loss using the
-        # downstream layer's output gradient
+        gradient_clip = 0.001
+
+        if self.use_batchnorm:
+            self.gamma_gradients = np.sum(gradient_output * self.normalized_output, axis=0)
+            self.beta_gradients = np.sum(gradient_output, axis=0)
+            gradient_output = gradient_output * self.gamma / np.sqrt(self.batch_var + 1e-7)
+
         self.weight_gradients = np.dot(self.input.T, gradient_output)
-        self.weight_gradients = np.clip(self.weight_gradients, -1.0, 1.0) # Apply gradient clipping to prevent exploding gradients
+        self.weight_gradients = np.clip(self.weight_gradients, -gradient_clip, gradient_clip)
 
-        # Compute the gradient of the bias with respect to the downstream layer's output
         self.bias_gradients = np.sum(gradient_output, axis=0, keepdims=True)
-        self.bias_gradients = np.clip(self.bias_gradients, -1.0, 1.0)
+        self.bias_gradients = np.clip(self.bias_gradients, -gradient_clip, gradient_clip)
 
-        # Compute the gradient of the output of the upstream layer which is connected to this layer with respect to the loss
         gradient_input = np.dot(gradient_output, self.weights.T)
-
-        # Return the gradient of the output of the upstream layer so that the gradients
-        # for weights in the upstream layer can be computed
         return gradient_input
 
 # Neural Network class
@@ -79,7 +108,8 @@ class NanoTensor:
             if index == len(self.layers) - 1:
                 x = linear(layer.forward(x))
             else:
-                x = relu(layer.forward(x))
+                # x = relu(layer.forward(x))
+                x = gelu(layer.forward(x))
                 # x = mactivation(layer.forward(x))
         return x
 
@@ -92,8 +122,8 @@ class NanoTensor:
             self.optimizer.update(layer)
 
 # Example Usage
-learning_rate = 0.001
-layers=[Layer(2, 5), Layer(5, 10), Layer(10,10), Layer(10, 5), Layer(5, 1)]
+learning_rate = 0.0005
+layers = [Layer(2, 5, use_batchnorm=True), Layer(5, 10, use_batchnorm=True), Layer(10, 10, use_batchnorm=True), Layer(10, 5, use_batchnorm=True), Layer(5, 1)]
 net = NanoTensor(layers, learning_rate)
 
 # XOR Example
@@ -101,7 +131,7 @@ input_data = np.array([[0, 1], [1, 0], [0, 0], [1, 1]])
 target = np.array([[1], [1], [0], [1]])
 
 # Training loop
-epochs = 500000
+epochs = 1000000
 
 for epoch in range(epochs):
     # Forward pass
